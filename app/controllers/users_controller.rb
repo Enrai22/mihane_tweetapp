6dbfca14510e051e220c394c3e43ab1e05cc8a73
Enrai22 <mihane.watanabe22@gmail.com>
2020-04-26 20:52:13 +0900

    20200426/2052

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user,{only:[:index,:show,:edit,:update]}
+  before_action :forbid_login_user,{only:[:new,:create,:login_form,:login]}
+  before_action :ensure_correct_user,{only:[:edit,:update]}
+
   def index
     @users=User.all
   end
@@ -15,10 +19,14 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      image_name:"tweeticon.png"
+      password: params[:password],
+      if params[:image]
+        image_name: params[:image]
+      else
+        image_name:"tweeticon.png"
     )
-    
     if @user.save
+      session[:user_id]=@user.id
       redirect_to("/users/#{@user.id}")
       flash[:notice]="ユーザー登録が完了しました"
     else
@@ -52,6 +60,37 @@ class UsersController < ApplicationController
    @user.destroy
      redirect_to("/users/index")
      flash[:notice]="ユーザー情報を削除しました"
+ end
+
+ def login_form
+ end
+
+ def login
+   @user=User.find_by(email: params[:email],
+                      password: params[:password])
+  if @user
+    session[:user_id]=@user.id
+    flash[:notice]="ログインしました"
+    redirect_to("/posts/index")
+  else
+    @error_message="メールアドレスまたはパスワードが間違っています"
+    @email=params[:email]
+    @password=params[:password]
+    render("users/login_form")
+  end
+ end
+
+ def logout
+   session[:user_id]=nil
+   flash[:notice]="ログアウトしました"
+   redirect_to("/login")
+ end
+
+ def ensure_correct_user
+   if @current_user.id != params[:id].to_i
+     flash[:notice]="権限がありません"
+     redirect_to("/posts/index")
+   end
  end
 
 end
